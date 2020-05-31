ef0c6950bb2d85a6d5cfeba6ecb094942b4abca1
Jakub Kap <jakub1996k@gmail.com>
2020-05-31 16:46:05 +0200

    Utworzenie synonimów

---
+++ relacyjna_bd/utworzone synonimy/synonimy.sql
@@ -0,0 +1,6 @@
+CREATE SYNONYM [dbo].[Album2] FOR [DESKTOP-P8PMK66\MSSQLSERVER2].[MusicShop].[dbo].[Album];
+CREATE SYNONYM [dbo].[Band2] FOR [DESKTOP-P8PMK66\MSSQLSERVER2].[MusicShop].[dbo].[Band];
+CREATE SYNONYM [dbo].[Instrument2] FOR [DESKTOP-P8PMK66\MSSQLSERVER2].[MusicShop].[dbo].[Instrument];
+CREATE SYNONYM [dbo].[Person2] FOR [DESKTOP-P8PMK66\MSSQLSERVER2].[MusicShop].[dbo].[Person];
+CREATE SYNONYM [dbo].[PersonInstrument2] FOR [DESKTOP-P8PMK66\MSSQLSERVER2].[MusicShop].[dbo].[PersonInstrument];
+CREATE SYNONYM [dbo].[Track2] FOR [DESKTOP-P8PMK66\MSSQLSERVER2].[MusicShop].[dbo].[Track];
